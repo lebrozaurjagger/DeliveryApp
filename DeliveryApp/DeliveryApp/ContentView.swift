69296439e63de1a901500bbbce950f0e8ba33201
Phillip <78912067+lebrozaurjagger@users.noreply.github.com>
2024-02-13 01:15:49 +0300

    Added main project files

---
+++ DeliveryApp/DeliveryApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeliveryApp
+//
+//  Created by Phillip on 13.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
