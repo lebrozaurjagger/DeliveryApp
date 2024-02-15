03c0289fba6303c931bb300888feac0abb76bca3
Phillip <78912067+lebrozaurjagger@users.noreply.github.com>
2024-02-15 23:47:01 +0300

    0.1.2

---
+++ DeliveryApp/DeliveryApp/Views/OrderViews/ListCartView.swift
@@ -0,0 +1,18 @@
+//
+//  ListCartView.swift
+//  DeliveryApp
+//
+//  Created by Phillip on 14.02.2024.
+//
+
+import SwiftUI
+
+struct ListCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListCartView()
+}
---
+++ DeliveryApp/DeliveryApp/Views/ProfileViews/NewCardView.swift
@@ -0,0 +1,18 @@
+//
+//  NewCardView.swift
+//  DeliveryApp
+//
+//  Created by Phillip on 14.02.2024.
+//
+
+import SwiftUI
+
+struct NewCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewCardView()
+}
